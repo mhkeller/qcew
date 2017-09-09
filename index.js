55d124829118e6d8b070f5517bb04db55fec4fbd
mhkeller <code@mhkeller.com>
2017-09-09 00:54:16 -0400

    export proper modules; update deps

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
-var request = require('request');
-var dsv			= require('dsv');
+var request = require('request')
+var dsv = require('d3-dsv')
 
 /* *******************************************************************************
 qcewRequestToData : This function takes a url, a callback and an optional format argument
-and returns data. By default, it will return json if `format` is not set. 
+and returns data. By default, it will return json if `format` is not set.
 If `'csv'` is passed as the format, it will return the data as it comes from the API,
 which is a csv.
 If `rows` is passed it will return an array of arrays, the first one being the header row.
-*/ 
+*/
 function qcewRequestToData(urlPath, callback, format){
-	var formatters = {
-		json: function(body){
-			return dsv.csv.parse(body);
-		},
-		csv: function(body){
-			return body;
-		},
-		rows: function(body){
-			var csvLines = body.split('\r\n');
-			var dataTable = [];
-			for (var i = 0; i < csvLines.length; i += 1) {
-				dataTable.push(csvLines[i].split(','));
-			}
-			return dataTable;
-		}
-	};
-	format = format || 'json';
-	request(urlPath, function(err, response, body){
-		var data;
-		if (!err && response.statusCode == 200){
-			data = formatters[format](body);
-			callback(null, data);
-		} else {
-			callback(err);
-		}
-	});
+  var formatters = {
+    json: function(body){
+      return dsv.csvParse(body);
+    },
+    csv: function(body){
+      return body;
+    },
+    rows: function(body){
+      var csvLines = body.split('\r\n');
+      var dataTable = [];
+      for (var i = 0; i < csvLines.length; i += 1) {
+        dataTable.push(csvLines[i].split(','));
+      }
+      return dataTable;
+    }
+  };
+  format = format || 'json';
+  request(urlPath, function(err, response, body){
+    var data;
+    if (!err && response.statusCode == 200){
+      data = formatters[format](body);
+      callback(null, data);
+    } else {
+      callback(err);
+    }
+  });
 }
 
 /* *******************************************************************************
 qcewGetAreaData : This function takes a year, quarter, and area argument and
 returns data containing the associated area data. Use 'a' for annual
-averages. 
+averages.
 For all area codes and titles see:
 http://www.bls.gov/cew/doc/titles/area/area_titles.htm
-*/ 
+*/
 function qcewGetAreaData(year, qtr, area, callback, format){
-	var urlPath = 'http://www.bls.gov/cew/data/api/'+year+'/'+qtr+'/area/'+area+'.csv';
-	qcewRequestToData(urlPath, callback, format);
+  var urlPath = 'http://www.bls.gov/cew/data/api/'+year+'/'+qtr+'/area/'+area+'.csv';
+  qcewRequestToData(urlPath, callback, format);
 }
 
 /* *******************************************************************************
 qcewGetIndustryData : This function takes a year, quarter, and industry code
-and returns data containing the associated industry data. Use 'a' for 
+and returns data containing the associated industry data. Use 'a' for
 annual averages. Some industry codes contain hyphens. The CSV files use
-underscores instead of hyphens. So 31-33 becomes 31_33. 
+underscores instead of hyphens. So 31-33 becomes 31_33.
 For all industry codes and titles see:
 http://www.bls.gov/cew/doc/titles/industry/industry_titles.htm
 */
 function qcewGetIndustryData(year, qtr, industry, callback, format){
-	var urlPath = 'http://www.bls.gov/cew/data/api/'+year+'/'+qtr+'/industry/'+industry+'.csv';
-	qcewRequestToData(urlPath, callback, format);
+  var urlPath = 'http://www.bls.gov/cew/data/api/'+year+'/'+qtr+'/industry/'+industry+'.csv';
+  qcewRequestToData(urlPath, callback, format);
 }
 
 /* *******************************************************************************
@@ -70,12 +70,12 @@ For all establishment size classes and titles see:
 http://www.bls.gov/cew/doc/titles/size/size_titles.htm
 */
 function qcewGetSizeData(year, size, callback, format){
-	var urlPath = 'http://www.bls.gov/cew/data/api/'+year+'/1/size/'+size+'.csv';
-	qcewRequestToData(urlPath, callback, format);
+  var urlPath = 'http://www.bls.gov/cew/data/api/'+year+'/1/size/'+size+'.csv';
+  qcewRequestToData(urlPath, callback, format);
 }
 
 module.exports = {
-	getAreaData: qcewGetAreaData,
-	getAreaData: qcewGetIndustryData,
-	getAreaData: qcewGetSizeData
+  getAreaData: qcewGetAreaData,
+  getIndustryData: qcewGetIndustryData,
+  getSizeData: qcewGetSizeData
 };
